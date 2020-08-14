73522df5351d16d21468eee8dbe88f65523dd8eb
ITLINFOSYS\dan.tudorin <dan.tudorin@infosysconsulting.com>
2020-08-14 12:30:34 +0300

    modify UI footer, remove route list-collection from navbar, redirect / to list-collection route, add 404 page not found component

--- admin-dashbord/src/app/app-routing.module.ts
+++ admin-dashbord/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CollectionListComponent } from './collection-list/collection-list.component';
 import { CreateCollectionComponent } from './create-collection/create-collection.component';
-
+import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
+  {
+    path : '',
+    redirectTo : 'allCollections',
+    pathMatch : 'full'
+  },
   {
     path : 'allCollections', 
     component : CollectionListComponent
@@ -11,6 +16,10 @@ const routes: Routes = [
   {
     path : 'create-collection',
     component : CreateCollectionComponent
+  },
+  {
+    path : '**',
+    component : NotFoundComponent
   }
 ];
 
--- admin-dashbord/src/app/app.module.ts
+++ admin-dashbord/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SpinnerComponent } from './spinner/spinner.component';
 import { CollectionItemComponent } from './collection-list/collection-item/collection-item.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FooterComponent } from './footer/footer.component';
     CollectionItemComponent,
     NavbarComponent,
     routingComponents,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
