b7a2a07fbd0f2842404e48c889369b3cd72ff58a
Larisa Popa <larisa.popa@ad.infosys.com>
2020-09-09 10:20:51 +0300

    postgres

--- backend/admin-service/src/main/java/com/infosys/admin/AdminService.java
+++ backend/admin-service/src/main/java/com/infosys/admin/AdminService.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.infosys.admin.model")
--- backend/gate-service/src/main/java/com/infosys/gate/GateServiceApplication.java
+++ backend/gate-service/src/main/java/com/infosys/gate/GateServiceApplication.java
@@ -1,6 +1,5 @@
 package com.infosys.gate;
 
-import com.infosys.admin.config.H2DatabaseConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
